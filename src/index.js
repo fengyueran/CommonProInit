81e09ad1bd41ec8a4deb60190a1d8dfd971f4c00
xinghunm <xinghunm@curacloudcorp.com>
2018-04-10 10:53:06 +0800

    add app entry

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import run from './app/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+run();
